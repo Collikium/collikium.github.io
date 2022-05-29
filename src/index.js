af9f8ac6a84ed3d88ec64e8aebf8c79ea69026a9
ENTL210 <edwardlamhitech@gmail.com>
2022-05-29 01:03:58 -0700

    Init Firebase

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyBgTaO4P1RT0CVeoPbXp23DZjoHEq2r7ow",
+    authDomain: "collikium.firebaseapp.com",
+    projectId: "collikium",
+    storageBucket: "collikium.appspot.com",
+    messagingSenderId: "100840388389",
+    appId: "1:100840388389:web:100df31ab1acae7e684ccc",
+    measurementId: "G-NPS1BBNB43"
+  });
+
+
+  
