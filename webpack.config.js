fdb3a1865cbf13727f22e677b661662149583fc2
ENTL210 <edwardlamhitech@gmail.com>
2022-05-29 21:12:58 -0700

    Update webpack.config,js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: 'bundle.js',
-        publicPath: ""
+        publicPath: "collikium.github.io"
     },
     watch: true
 }
