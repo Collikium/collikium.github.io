fc25ef6b9f32fd30123cc21d1b2d449157e40f2d
ENTL210 <edwardlamhitech@gmail.com>
2022-06-04 13:46:14 -0700

    Add Firebase auth emulator

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 // Import the functions from the SDKs
 import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  connectAuthEmulator,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 
 // Firebase's configuration
-const firebaseConfig = {
+const firebaseApp = {
   apiKey: "AIzaSyBgTaO4P1RT0CVeoPbXp23DZjoHEq2r7ow",
   authDomain: "collikium.firebaseapp.com",
   projectId: "collikium",
@@ -12,7 +17,10 @@ const firebaseConfig = {
   measurementId: "G-NPS1BBNB43"
 };
 
-// Initialize Firebase
-initializeApp(firebaseConfig) 
+// Initialize auth
+const auth = getAuth(firebaseApp);
+connectAuthEmulator(auth, "http://localhost:9099");
 
-
+const loginEmailPassword = async () => {
+  
+}
